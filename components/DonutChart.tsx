d8f6e77a996c44d327a6c68cb86e6c08c7fbe0e4
joselourenco <3s3phinxil@gmail.com>
2024-04-08 21:42:10 -0300

    chore: fixing build imports

--- components/DonutChart.tsx
+++ components/DonutChart.tsx
@@ -1,13 +1,12 @@
 'use client'
-
-import React, { Component, useEffect } from 'react';
-import Chart from 'react-apexcharts'
+import dynamic from "next/dynamic";
+const ApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 export default function DonutChart(props: { state: { options: ApexCharts.ApexOptions, series: number[] } }) {
 
     return (
         <div className="donut">
-            <Chart
+            <ApexChart
                 options={props.state.options || {}}
                 series={props.state.series ||  [44, 55, 41, 17, 15]}
                 type="donut"
