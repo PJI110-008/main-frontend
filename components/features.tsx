ffb81dd611c60a8a028fcc1c6910404cc75a36e2
José Lourenço <3s3phinxil@gmail.com>
2024-05-06 20:32:08 -0300

    fix: removing unused text

--- components/features.tsx
+++ components/features.tsx
@@ -5,14 +5,14 @@ export default function Features() {
         <div className="py-12 md:py-20">
 
           {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+          {/* <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 mb-4">
               Algum texto bacana aqui
             </h2>
             <p className="text-xl text-gray-400">
               Outro texto bacana aqui
             </p>
-          </div>
+          </div> */}
 
           {/* Items */}
           <div className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
